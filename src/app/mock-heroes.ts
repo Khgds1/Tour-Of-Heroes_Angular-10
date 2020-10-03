5f2c2b6731d58a7d1e9442bb71d4e119b9495a84
khgds1 <khgds1@hotmail.com>
2020-10-03 10:26:44 -0300

    tour dos herois

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Flash' },
+    { id: 12, name: 'Ravena' },
+    { id: 13, name: 'Estelar' },
+    { id: 14, name: 'Mulher Maravilha' },
+    { id: 15, name: 'Superman' },
+    { id: 16, name: 'Batgirl' },
+    { id: 17, name: 'Supergirl' },
+    { id: 18, name: 'Valdeci' },
+    { id: 19, name: 'Batman' },
+    { id: 20, name: 'Cyborg' }
+]
